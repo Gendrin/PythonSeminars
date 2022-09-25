d4ea3bf64436ef634481d4e8ec7395366b4b0be7
Gendrin <Gendrinvl@gmail.com>
2022-09-26 02:02:47 +1000

    Task 1

--- Seminar1/Seminar/Seminar1/Seminar1.py
+++ Seminar1/Seminar/Seminar1/Seminar1.py
@@ -1,1 +1,10 @@
-# Test push
+#1. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+#dayWeek = int(input('Введите число соответствующее дню недели'))
+
+#Task number 1
+dayWeek = int(input('Insert number for week day '))
+
+if dayWeek == 6 or dayWeek == 7:
+   print(f'- {dayWeek} -> yes')
+elif dayWeek <= 0 or dayWeek < 6:
+   print(f'- {dayWeek} -> no')
