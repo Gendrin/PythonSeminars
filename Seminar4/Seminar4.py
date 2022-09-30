807077982c9703ab94ece8e1b5e2f0902c1482af
Gendrin <Gendrinvl@gmail.com>
2022-09-30 18:03:21 +1000

    Seminar4 Task1

---
+++ Seminar4/Seminar4.py
@@ -0,0 +1,7 @@
+import math
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print('Task1 Seminar N4')
+
+d = int(input('Введите точность числа Пи '))
+print(f'Число Пи с заданной точностью {d} равно {round(math.pi,d)}')
