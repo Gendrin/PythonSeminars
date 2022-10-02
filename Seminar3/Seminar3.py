4f13a7a4503e26eedf550650038445fc4a034a61
Sergei Zakharchuk <108308489+Gendrin@users.noreply.github.com>
2022-10-02 21:10:09 +1000

    Change Task#4

--- Seminar3/Seminar3.py
+++ Seminar3/Seminar3.py
@@ -19,11 +19,13 @@ def CreateRandomListU(rangeList):
         result.append(round(random.uniform(0, 9), 2))
     print(result, end=' ')
     return result
+
 def SumOddDigits(listForSum):
     result=0
     for i in range(1,len(listForSum),2):
         result+=listForSum[i]
     return result
+
 def FindSumPair(taskList):
     if len(taskList)%2!=0:
         rangeMult = len(taskList) // 2 + 1
@@ -38,9 +40,18 @@ def FindSumPair(taskList):
             result.append(taskList[i])
         j-=1
     return result
+
 def FindRaznOdd(taskListOdd):
     listOddMinZero = [round(i % 1.0, 2) for i in taskListOdd if i % 1 != 0]
     return round(max(listOddMinZero) - min(listOddMinZero), 2)
+
+def BinFormatGo(number):
+    binList = []
+    if number > 0:
+        while number!=0:
+            binList.insert(0,number%2)
+            number //=2
+    return binList
 
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт
 # сумму элементов списка, стоящих на нечётной позиции.
@@ -69,16 +80,10 @@ if rangeTsk3 != None:
     tsk3List = CreateRandomListU(rangeTsk3)
     print(f' --> {FindRaznOdd(tsk3List)}')
 
-# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 print('Seminar3 Task N4')
-N = int(input('Введите преобразуемое в двоичный формат положительное десятичное число, результат строка! --> '))
-N = CheckInputInt(N)
-if N > 0:
-    result = ''
-    while N!=0:
-        result = str(N%2) + result
-        N //=2
-print(result)
+numberTask4 = int(input('Введите преобразуемое в двоичный формат положительное десятичное число, результат список! --> '))
+numberTask4 = CheckInputInt(numberTask4)
+print(f'{numberTask4} --> {BinFormatGo(numberTask4)}')
 
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 print('Seminar3 Task N5')
