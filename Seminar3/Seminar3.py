afe68c319539f2c89b262f497ed2f63066739d35
Gendrin <Gendrinvl@gmail.com>
2022-10-02 14:59:59 +1000

    Seminar3 Task3 коррекция после просмотра семинара

--- Seminar3/Seminar3.py
+++ Seminar3/Seminar3.py
@@ -12,6 +12,13 @@ def CreateRandomList(rangeList):
     result = sample(range(rangeList*2), rangeList)
     print(result,end=' ')
     return result
+
+def CreateRandomListU(rangeList):
+    result = []
+    for i in range(rangeList):
+        result.append(round(random.uniform(0, 9), 2))
+    print(result, end=' ')
+    return result
 def SumOddDigits(listForSum):
     result=0
     for i in range(1,len(listForSum),2):
@@ -31,6 +38,9 @@ def FindSumPair(taskList):
             result.append(taskList[i])
         j-=1
     return result
+def FindRaznOdd(taskListOdd):
+    listOddMinZero = [round(i % 1.0, 2) for i in taskListOdd if i % 1 != 0]
+    return round(max(listOddMinZero) - min(listOddMinZero), 2)
 
 # Задайте список из нескольких чисел. Напишите программу, которая найдёт
 # сумму элементов списка, стоящих на нечётной позиции.
@@ -49,23 +59,15 @@ if rangeTsk2 != None:
     tsk2List = CreateRandomList(rangeTsk2)
     print(f' --> {FindSumPair(tsk2List)}')
 
-
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным
 # значением дробной части элементов.
 # Осталось добавить формирование рандом списка
 print('Seminar3 Task N3')
-
 #lst = [1.1, 1.2, 3.1, 5, 10.01]
-N = input("Enter long create random list -> ")
-N = CheckInputInt(N)
-if N != None:
-    lst = []
-    for i in range(N):
-        lst.append(round(random.uniform(0, 9),2))
-    print(f'Create random list -> {lst}')
-    new_lst = [round(i%1.0,2) for i in lst if i%1!=0]
-    print(f'Result work list -> {new_lst} -- max value -> {max(new_lst)} -- min value -> {min(new_lst)}')
-    print('Result', '->', round(max(new_lst) - min(new_lst),2))
+rangeTsk3 = CheckInputInt(input("Enter range list for Task3 -> "))
+if rangeTsk3 != None:
+    tsk3List = CreateRandomListU(rangeTsk3)
+    print(f' --> {FindRaznOdd(tsk3List)}')
 
 # Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 print('Seminar3 Task N4')
