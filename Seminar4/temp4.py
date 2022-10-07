48f1428331eaddd2b6a7439060bee7a5e0dfca8e
Gendrin <Gendrinvl@gmail.com>
2022-10-07 15:12:35 +1000

    Seminar4 Task4

--- Seminar4/temp4.py
+++ Seminar4/temp4.py
@@ -45,52 +45,20 @@ def CreateStringPoly(inNum):
                     resultString += CreateRandomSign()+str(fierstList[i]) + '=0'
             i+=1
         return resultString
-        #task4File = open("Task4.txt", "a")
-        #task4File.write(resultString+'\n')
-        #print(resultString)
 
 
-print('Task4 Seminar N4')
-#inNumTsk5 = int(input("Enter a real number: -> "))
-insertString = input("Enter numbers separated by spaces -> ")
-arrString=insertString.split()
-for j in range(0,len(arrString)):
-    inNumTsk5=CheckInputInt(arrString[j])
-    if inNumTsk5!=None and inNumTsk5!=0 and inNumTsk5 > 0:
-        fierstList = CreateRandomList(0,9,inNumTsk5+1)
-        print(f'Creating random List -> {fierstList}')
-        i = 0
-        resultString=''
-        saveInNum=inNumTsk5
-        inNumTsk5+=1
-        flagZnak=False
-        while i < len(fierstList):
-            if fierstList[i]!=0 and i < saveInNum:
-                inNumTsk5=inNumTsk5-1
-                if flagZnak:
-                    resultString += CreateRandomSign()+str(fierstList[i]) + '*x^' + str(inNumTsk5)  # + CreateRandomSign()
-                else:
-                    resultString += str(fierstList[i]) + '*x^' + str(inNumTsk5)
-                    flagZnak=True
-            else:
-                inNumTsk5 = inNumTsk5 - 1
-            if i == saveInNum:
-                if fierstList[i]==0:
-                    resultString +='=0'
-                else:
-                    resultString += CreateRandomSign()+str(fierstList[i]) + '=0'
-            i+=1
-        task4File = open("Task4.txt", "a")
-        task4File.write(resultString+'\n')
-        task4File.close
-        print(resultString)
 
 print('Task4 Seminar N4 Fun')
 insertString = input("Enter numbers separated by spaces -> ")
 arrString=insertString.split()
 for j in range(0,len(arrString)):
     inNumTsk5=CheckInputInt(arrString[j])
-    print(CreateStringPoly(inNumTsk5))
+    resultString=CreateStringPoly(inNumTsk5)
+    task4File = open("Task4.txt", "a")
+    task4File.write(resultString + '\n')
+    task4File.close
+    print('writing in file Task4.txt -->' + resultString)
+
 # 1. Формируем список коэффициентов 10 штук ф-ия
 # 2. Формируем строку , первый К всегда положительный, следующие
 # добавляют себе знак, если к =0 пропуск элемента.
