0d6888c2ef0829c5f2ae2e0da662835af1a244fb
Gendrin <Gendrinvl@gmail.com>
2022-10-14 22:30:20 +1000

    Seminar5 Task1 and Task2

--- Seminar4/temp4.py
+++ Seminar4/temp4.py
@@ -1,4 +1,6 @@
 from random import randint
+from random import sample
+from random import choice
 
 def CheckInputInt(testInput):
     try:
@@ -87,31 +89,53 @@ def ConcatePolyFiles(inSeparateString):
 #     WritingFile(insertStringNameFile,resultString)
 #     print('writing in file Task4.txt -->' + resultString)
 
-print('Task4 Seminar N5 ')
-insertString = input("Enter names files number1 and number2 separated by spaces poly1.txt and poly2.txt -> ")
-ConcatePolyFiles(insertString)
-# arrString=insertString.split()
-# for j in range(0,len(arrString)):
-#     poly1 = arrString[0]
-#     poly2 = arrString[1]
-# with open(poly1, "r") as file1:
-#     contents1 = file1.readlines()
-# with open(poly2, "r") as file2:
-#     contents2 = file2.readlines()
-# if len(contents1) == len(contents2):
-#     #print('Result')
-#     insertStrFileResult = input("Enter names files result Task5 polyRez.txt -> ")
-#     for i in range(0,len(contents1)):
-#         concateStr=ConcatePolyStr(contents1[i],contents2[i])
-#         WritingFile(insertStrFileResult,concateStr)
-# else:
-#     print('The contents of the files do not match!')
+# print('Task4 Seminar N5 ')
+# insertString = input("Enter names files number1 and number2 separated by spaces poly1.txt and poly2.txt -> ")
+# ConcatePolyFiles(insertString)
+
+# num_list = range(0,10)
+# for i in range(10,1,-1):
+#     #value = choice(num_list)
+#     #value = choice(range(0,10))
+#     value = choice('0123456789')
+#     print(value,end="")
+def Generate_List_Random_Worlds(count_w:int, text:str='абв'):
+    Random_world_list=[]
+    for i in range(count_w):
+        world=sample(text,3)
+        Random_world_list.append("".join(world))
+    return " ".join(Random_world_list)
 
 
+print('Task1 Seminar N5')
+insertNumberWorlds = CheckInputInt(input('Enter number rendom worlds for task N1: '))
+insertFierstRandomString = input('Enter start rendom string for task N1: ')
+if insertNumberWorlds!=None:
+    if insertFierstRandomString!=None:
+        result_generate_string=Generate_List_Random_Worlds(insertNumberWorlds,insertFierstRandomString)
 
-#readlines(): считывает все строки файла в список
 
-# 1. Формируем список коэффициентов 10 штук ф-ия
-# 2. Формируем строку , первый К всегда положительный, следующие
-# добавляют себе знак, если к =0 пропуск элемента.
-# 3. Ф-ия формирующая знак.
+str,num,temp_str='абв',20,''
+result_str=''
+my_list=[]
+list_char=list(str)
+for i in range(num):
+    world = sample(str,3)
+    my_list.append(''.join(world))
+print(my_list)
+result_string=' '.join(my_list)
+print(' '.join(my_list))
+result_string = result_string.replace(" абв","")
+print(result_string)
+# list_num = [1,1,1,2,2,2,3,3,3,4,5,5,6,7]
+# dict = {}.fromkeys(list_num,0)
+# print(dict)
+# for i in list_num:
+#     dict[i] +=1
+# print(dict)
+# result=[]
+# for k in dict:
+#     if dict[k] == 1:
+#         result.append(k)
+# print(result)
+
---
+++ Seminar5/Seminar5.py
@@ -0,0 +1,131 @@
+from random import sample
+
+
+def CheckInputInt(testInput):
+    try:
+        return int(testInput)
+    except ValueError as Err:
+        print(f'Incorrect data entry -> {Err}')
+        return None
+
+
+def Generate_List_Random_Worlds(count_w: int, text: str = 'абв'):
+    Random_world_list = []
+    for i in range(count_w):
+        world = sample(text, len(text))
+        Random_world_list.append("".join(world))
+    return " ".join(Random_world_list)
+
+
+def Replase_String(inString, baseString: str = 'абв'):
+    result = inString.replace(" " + baseString, "")
+    result = result.replace(baseString + " ", "")
+    return result
+
+def WritingFile(nameFile,printString):
+    nameFile=open(nameFile, "a")
+    nameFile.write(printString + '\n')
+    nameFile.close
+
+def SetCryptoFiles(inSeparateStringNameFiles):
+    arrString = inSeparateStringNameFiles.split()
+    fileList=[]
+    for i in range(0, len(arrString)):
+        fileList.append(arrString[i]) #  Name files - 0 - fileInText, 1- fileCryptText, 2 - file DeCryptText
+    return fileList
+
+def InStringsToFile(nameFile):
+    numString =  input("Enter number string for input text_worlds.txt -> ")
+    if numString.isdigit():
+        for i in range(0,int(numString)):
+            insertStrFileIn = input("Enter text for file Task2 Seminar5 text for coding text_worlds.txt -> ")
+            WritingFile(nameFile,insertStrFileIn)
+
+def PrintstringInFile(nameFile):
+    with open(nameFile, "r") as fileForRead:
+        contents = fileForRead.readlines()
+        for i in range(0, len(contents)):
+            print(contents[i],end='')
+
+def CodingOneString(textString):
+    inText = textString.strip()
+    lstr = list(inText)
+    my_dict = {}.fromkeys(lstr, 0)
+    for i in lstr:
+        my_dict[i] += 1
+    crypt_str = ''
+    for j in my_dict:
+        crypt_str += str(my_dict[j]) + j
+    return crypt_str
+
+def DecodingOneString(codeString):
+    inText = codeString.strip()
+    dc_list = list(inText)
+    key_list = []
+    val_list = []
+    i=0
+    while i <  len(dc_list):
+        temp_value=''
+        if dc_list[i].isdigit():
+            while dc_list[i].isdigit():
+                temp_value+=dc_list[i]
+                i+=1
+            val_list.append(int(temp_value))
+        else:
+            key_list.append(dc_list[i])
+            i+=1
+
+    dec_dict = dict(zip(key_list, val_list))
+    dec_str = ''
+    for k in dec_dict:
+        dec_str += dec_dict[k] * k
+    return dec_str
+
+def CodingTextFiles(textFile,codeFiles):
+    with open(textFile, "r") as fileT:
+        contentsText = fileT.readlines()
+    with open(codeFiles, "w") as fileC:
+        for i in range(0, len(contentsText)):
+            WritingFile(codeFiles,CodingOneString(contentsText[i]))
+
+def DecodingFile(codeFile,resultFile):
+    with open(codeFile, "r") as fileC:
+        contentsCode = fileC.readlines()
+    with open(resultFile, "w") as fileT:
+        for i in range(0, len(contentsCode)):
+            WritingFile(resultFile, DecodingOneString(contentsCode[i]))
+
+
+
+
+print('Task1 Seminar N5')
+insertNumberWorlds = CheckInputInt(input('Enter number rendom worlds for task N1: '))
+insertFierstRandomString = input('Enter start rendom string for task N1: ')
+if insertNumberWorlds != None:
+    if insertFierstRandomString == '':
+        result_generate_string = Generate_List_Random_Worlds(insertNumberWorlds)
+        print(result_generate_string)
+        print(Replase_String(result_generate_string))
+    else:
+        result_generate_string = Generate_List_Random_Worlds(insertNumberWorlds, insertFierstRandomString)
+        print(result_generate_string)
+        print(Replase_String(result_generate_string, insertFierstRandomString))
+
+
+
+print('Task2 Seminar N5')
+fileNameList = []
+print('Введите имена файлов исходного текста, кодирования и декодирования, разделенные пробелом text_worlds.txt ->')
+print('Или нажмите ввод если вас устраивают файлы по умолчанию')
+fileNameList = SetCryptoFiles(input("text_words.txt text_code_words.txt text_decode_words.txt "))
+if len(fileNameList)==0:
+    fileNameList = SetCryptoFiles('text_words.txt text_code_words.txt text_decode_words.txt')
+InStringsToFile(fileNameList[0])
+print(f'Out in File - > {fileNameList[0]}')
+PrintstringInFile(fileNameList[0])
+CodingTextFiles(fileNameList[0],fileNameList[1])
+print(f'Coding File - > {fileNameList[1]}')
+PrintstringInFile(fileNameList[1])
+DecodingFile(fileNameList[1],fileNameList[2])
+print(f'DeCoding File - > {fileNameList[2]}')
+PrintstringInFile(fileNameList[2])
