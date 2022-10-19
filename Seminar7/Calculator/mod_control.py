09d64337341ca7e6ee7d1cdf29068001c3419bba
Gendrin <Gendrinvl@gmail.com>
2022-10-19 17:42:49 +1000

    Seminar7 All functions realizations

--- Seminar7/Calculator/mod_control.py
+++ Seminar7/Calculator/mod_control.py
@@ -23,5 +23,20 @@ def Operation(code_op):
         resultsum = mmult.mult(mint.get_valueC(), mint.get_valueC())  # Меню 2-3 +
         mint.view_data("Результат умножения комплексных чисел", resultsum)
     elif code_op == '24':
-        resultsum = mdiv.div(mint.get_valueC(), mint.get_valueC())  # Меню 2-4 +
+        resultsum = mdiv.div(mint.get_valueC(), mint.get_valueC())  # Меню 2- 4 +
         mint.view_data("Результат деления комплексных чисел", resultsum)
+    elif code_op == '31':
+        resultsum = mdiv.div(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 1 +
+        mint.view_data("Результат деления рациональных чисел чисел", resultsum)
+    elif code_op == '32':
+        resultsum = mdiv.IntDiv(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 2 +
+        mint.view_data("Результат целочисленного деления рациональных чисел чисел", resultsum)
+    elif code_op == '33':
+        resultsum = mdiv.RemDiv(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 3 +
+        mint.view_data("Результат остатка деления рациональных чисел чисел", resultsum)
+    elif code_op == '15':
+        resultsum = mmult.dpow(mint.get_valueR(), mint.get_valueR())  # Меню 1 5 +
+        mint.view_data("Результат возведения в степень рационального числа", resultsum)
+    elif code_op == '16':
+        resultsum = mmult.dsqrt(mint.get_valueR())  # Меню 1 5 +
+        mint.view_data("Результат вычисления корня рационального числа", resultsum)
