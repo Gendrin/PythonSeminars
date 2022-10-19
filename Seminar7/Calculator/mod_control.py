9c182e9a9321e9dd67d9eea2fc038eec9ef009ac
Gendrin <Gendrinvl@gmail.com>
2022-10-19 19:39:48 +1000

    Seminar7 correct mod_control for loggs

--- Seminar7/Calculator/mod_control.py
+++ Seminar7/Calculator/mod_control.py
@@ -3,40 +3,76 @@ import model_sum as msum
 import model_sub as msub
 import model_mult as mmult
 import model_div as mdiv
+
 def Operation(code_op):
+    a,b=0,0
+    infString=''
+    if code_op == '11' or code_op == '12' or code_op == '13' or code_op == '15'\
+            or code_op == '31' or code_op == '32' or code_op == '33':
+        a, b = mint.get_valueR(), mint.get_valueR()
+    if code_op == '16':
+        a = mint.get_valueR()
+    if code_op == '21' or code_op == '22' or code_op == '23' or code_op == '24':
+            a, b = mint.get_valueC(), mint.get_valueC()
+
+
     if code_op == '11':
-        resultsum = msum.sum(mint.get_valueR(),mint.get_valueR())# Меню 1-1 +
-        mint.view_data("Результат сложения рациональных",resultsum)
+        resultsum = msum.sum(a,b)# Меню 1-1 +
+        infString='Результат сложения рациональных чисел a -> '+str(a)+' и b -> '+str(b)
+        # Пишем в лог
+        mint.view_data(infString, resultsum)
     elif code_op =='21':
-        resultsum = msum.sum(mint.get_valueC(), mint.get_valueC())  # Меню 2-1 +
-        mint.view_data("Результат сложения комплексных чисел", resultsum)
+        resultsum = msum.sum(a, b)  # Меню 2-1 +
+        infString = 'Результат сложения комплексных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op =='12':
-        resultsum = msub.sub(mint.get_valueR(), mint.get_valueR())  # Меню 1-2 +
-        mint.view_data("Результат вычитания рациональных чисел", resultsum)
+        resultsum = msub.sub(a, b)  # Меню 1-2 +
+        infString = 'Результат вычитания рациональных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op =='22':
-        resultsum = msub.sub(mint.get_valueC(), mint.get_valueC())  # Меню 2-2 +
-        mint.view_data("Результат вычитания комплексных чисел", resultsum)
+        resultsum = msub.sub(a, b)  # Меню 2-2 +
+        infString = 'Результат вычитания комплексных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '13':
-        resultsum = mmult.mult(mint.get_valueR(), mint.get_valueR())  # Меню 1-3 +
-        mint.view_data("Результат умножения рациональных чисел", resultsum)
+        resultsum = mmult.mult(a, b)  # Меню 1-3 +
+        infString = 'Результат умножения рациональных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '23':
-        resultsum = mmult.mult(mint.get_valueC(), mint.get_valueC())  # Меню 2-3 +
-        mint.view_data("Результат умножения комплексных чисел", resultsum)
+        resultsum = mmult.mult(a, b)  # Меню 2-3 +
+        infString = 'Результат умножения комплексных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '24':
-        resultsum = mdiv.div(mint.get_valueC(), mint.get_valueC())  # Меню 2- 4 +
-        mint.view_data("Результат деления комплексных чисел", resultsum)
+        resultsum = mdiv.div(a, b)  # Меню 2- 4 +
+        infString = 'Результат деления комплексных чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '31':
-        resultsum = mdiv.div(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 1 +
-        mint.view_data("Результат деления рациональных чисел чисел", resultsum)
+        resultsum = mdiv.div(a, b)  # Меню 1 3 1 +
+        infString = 'Результат деления рациональных чисел чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '32':
-        resultsum = mdiv.IntDiv(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 2 +
-        mint.view_data("Результат целочисленного деления рациональных чисел чисел", resultsum)
+        resultsum = mdiv.IntDiv(a, b)  # Меню 1 3 2 +
+        infString = 'Результат целочисленного деления рациональных чисел чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '33':
-        resultsum = mdiv.RemDiv(mint.get_valueR(), mint.get_valueR())  # Меню 1 3 3 +
-        mint.view_data("Результат остатка деления рациональных чисел чисел", resultsum)
+        resultsum = mdiv.RemDiv(a, b)  # Меню 1 3 3 +
+        infString = 'Результат остатка деления рациональных чисел чисел a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '15':
-        resultsum = mmult.dpow(mint.get_valueR(), mint.get_valueR())  # Меню 1 5 +
-        mint.view_data("Результат возведения в степень рационального числа", resultsum)
+        resultsum = mmult.dpow(a, b)  # Меню 1 5 +
+        infString = 'Результат возведения в степень рационального числа a -> ' + str(a) + ' и b -> ' + str(b)
+
+        mint.view_data(infString, resultsum)
     elif code_op == '16':
-        resultsum = mmult.dsqrt(mint.get_valueR())  # Меню 1 5 +
-        mint.view_data("Результат вычисления корня рационального числа", resultsum)
+        resultsum = mmult.dsqrt(a)  # Меню 1 5 +
+        infString = 'езультат вычисления корня рационального числа a -> ' + str(a)
+
+        mint.view_data(infString, resultsum)
